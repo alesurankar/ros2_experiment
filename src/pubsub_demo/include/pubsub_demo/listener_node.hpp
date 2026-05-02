e4227e4630ef83a7f7261b1224057f82b79522c1
Aleš <ale.urankar@gmail.com>
2026-05-02 14:23:51 +0200

    listener_node

--- src/pubsub_demo/include/pubsub_demo/listener_node.hpp
+++ src/pubsub_demo/include/pubsub_demo/listener_node.hpp
@@ -6,4 +6,7 @@ class ListenerNode : public rclcpp::Node
 {
 public:
   ListenerNode();
+private:
+  void topicCallback(const std_msgs::msg::String::SharedPtr msg);
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
--- src/pubsub_demo/src/listener_node.cpp
+++ src/pubsub_demo/src/listener_node.cpp
@@ -4,5 +4,17 @@
 using namespace std::chrono_literals;
 
 ListenerNode::ListenerNode()
+  : 
+  Node("listener_node")
 {
+  subscription_ = this->create_subscription<std_msgs::msg::String>(
+    "chatter",
+    10,
+    std::bind(&ListenerNode::topicCallback, this, std::placeholders::_1)
+  );
+}
+
+void ListenerNode::topicCallback(const std_msgs::msg::String::SharedPtr msg)
+{
+  RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
 }
