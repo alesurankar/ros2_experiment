eb48971cf1ee2c574bad519710432a1078c2f186
Aleš <ale.urankar@gmail.com>
2026-05-08 17:54:36 +0200

    move robot

--- src/motion_test/src/motion_commander.cpp
+++ src/motion_test/src/motion_commander.cpp
@@ -5,6 +5,8 @@
 #include <unistd.h>   // linux system functions provider
 #include <fcntl.h>    // for nonblocking keyboard in linux
 #include <cstdio>
+#include <algorithm>
+#include <cmath>
 
 
 using namespace std::chrono_literals;
@@ -31,7 +33,7 @@ MotionCommander::MotionCommander()
   );
 
   timer_ = this->create_wall_timer(
-    500ms,
+    50ms,
     [this]() { this->timerCallback(); }
   );
 
@@ -47,11 +49,11 @@ void MotionCommander::timerCallback()
   msg.linear_x = linear_x_;
   msg.angular_z = angular_z_;
 
-  // Logging ///
-  RCLCPP_INFO(this->get_logger(),
-    "Publishing: linear_x=%.2f angular_z=%.2f",
-    msg.linear_x, msg.angular_z);
-  //////////////
+  // // Logging ///
+  // RCLCPP_INFO(this->get_logger(),
+  //   "Publishing: linear_x=%.2f angular_z=%.2f",
+  //   msg.linear_x, msg.angular_z);
+  // //////////////
 
   publisher_->publish(msg);
 }
@@ -68,46 +70,79 @@ void MotionCommander::keyboardLoop()
 
   fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
 
+  bool w_pressed = false;
+  bool s_pressed = false;
+  bool a_pressed = false;
+  bool d_pressed = false;
+
   while (rclcpp::ok()) {
 
-    int c = getchar();
+    int c;
 
-    if (c == EOF) {
-      std::this_thread::sleep_for(50ms);
-      continue;
-    }
+    w_pressed = false;
+    s_pressed = false;
+    a_pressed = false;
+    d_pressed = false;
 
-    switch (c) {
-
-      case 'w':
-        linear_x_ += 0.1;
-        break;
-
-      case 's':
-        linear_x_ -= 0.1;
-        break;
-
-      case 'a':
-        angular_z_ += 0.1;
-        break;
-
-      case 'd':
-        angular_z_ -= 0.1;
-        break;
-
-      case ' ':
+    while ((c = getchar()) != EOF) {
+      if (c == 'w') {
+        w_pressed = true;
+      }
+      if (c == 's') {
+        s_pressed = true;
+      }
+      if (c == 'a') {
+        a_pressed = true;
+      }
+      if (c == 'd') {
+        d_pressed = true;
+      }
+      if (c == ' ') {
         linear_x_ = 0.0;
         angular_z_ = 0.0;
-        break;
+      }
     }
 
-    RCLCPP_INFO(
-      this->get_logger(),
-      "linear=%.2f angular=%.2f",
-      linear_x_,
-      angular_z_
-    );
-  }
+    // linear control
+    if (w_pressed) {
+      linear_x_ += 0.11;
+    }
+    else if (linear_x_ > 0.0) {
+      linear_x_ -= 0.11;
+    }
+    if (s_pressed) {
+      linear_x_ -= 0.11;
+    }
+    else if (linear_x_ < 0.0) {
+      linear_x_ += 0.11;
+    }
 
+    // angular control
+    if (a_pressed) {
+      angular_z_ += 0.1;
+    }
+    else if (angular_z_ > 0.0) {
+      angular_z_ -= 0.1;
+    }
+    if (d_pressed) {
+      angular_z_ -= 0.1;
+    }
+    else if (angular_z_ < 0.0) {
+      angular_z_ += 0.1;
+    }
+
+    // clamp values
+    linear_x_ = std::clamp(linear_x_, -1.0, 1.0);
+    angular_z_ = std::clamp(angular_z_, -1.0, 1.0);
+    
+    // deadzone
+    if (std::abs(linear_x_) < 0.1) {
+      linear_x_ = 0.0;
+    }
+    if (std::abs(angular_z_) < 0.1) {
+      angular_z_ = 0.0;
+    }
+    std::this_thread::sleep_for(50ms);
+  }
   tcsetattr(STDIN_FILENO, TCSANOW, &old_tio);
 }
